bdd2c94d5118657d8049eef6a2d425c1d3ab6e96
Mafoya1er <marcel.aloekpo@epitech.eu>
2025-10-21 01:21:46 +0200

    Fix readthedocs.yml for new syntax

--- docs/conf.py
+++ docs/conf.py
@@ -16,6 +16,10 @@ author = 'Marcel ALOEKPO'
 release = '0.1.1'
 
 
+
+
+# -- Options for HTML output -------------------------------------------------
+
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.viewcode',
@@ -30,7 +34,11 @@ templates_path = ['_templates']
 
 exclude_patterns = []
 
+html_theme = 'sphinx_rtd_theme'
+
 html_static_path = ['_static']
+
+
 
 
 
