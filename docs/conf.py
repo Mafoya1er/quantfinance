c64b2642a495c618157a9b28314b4f3baf969f5f
Mafoya1er <marcel.aloekpo@epitech.eu>
2025-10-21 00:58:01 +0200

    Fix readthedocs.yml for new syntax

--- docs/conf.py
+++ docs/conf.py
@@ -19,8 +19,10 @@ release = '0.1.1'
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.viewcode',
-    'sphinx.ext.napoleon',
-    'myst_parser', 
+    'sphinx.ext.napoleon',  # Pour docstrings Google/NumPy style
+    'sphinx.ext.intersphinx',  # Pour liens vers d'autres docs
+    'myst_parser',  # Si tu utilises Markdown
+    'sphinx_copybutton',  # Pour copier le code
 ]
 
 templates_path = ['_templates']
@@ -28,5 +30,10 @@ templates_path = ['_templates']
 
 exclude_patterns = []
 
-
 html_static_path = ['_static']
+
+
+
+# Pour copier le code
+copybutton_prompt_text = ">>> "
+copybutton_prompt_is_regexp = True
